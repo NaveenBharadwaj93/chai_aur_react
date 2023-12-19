59297557bb17c0d8b13adce584825e82f6271c54
Naveen <naveen.satish24@gmail.com>
2023-12-19 14:27:26 +0530

    some changes

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Chai aur react | Naveen Bharadwaj</h1>
+        <Chai />
+    </div>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from './Chai'
+
+
+function App() {
+
+
+  return (
+    // added this fragment to add multiple html elements. jsx allows only one element.
+    <> 
+      <h1>Chai aur react with vite | NB</h1>
+      <Chai />
+    </>
+  )
+}
+
+export default App
